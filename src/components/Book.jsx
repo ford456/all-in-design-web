ae29a3d5a9066a124569e1e16bb0ca1ef8b65803
ford456 <fordllawat@gmail.com>
2025-07-01 11:00:54 +0700

    Update Book.jsx

--- src/components/Book.jsx
+++ src/components/Book.jsx
@@ -5,7 +5,7 @@ export default function Book() {
     <div className='bg-white pb-10 md:mb-120 h-auto md:h-[37.5rem] md:mt-10  items-center'>
       <div className=' container mx-auto max-w-[1320px] p-5 m-5'>
         <div className='text-center my-10'>
-            <h3 className=' cursor-default text-[2rem] leading-[1.25] font-medium text-[#FFFFF] justify-center items-center  '>สูติบัตร</h3>
+            <h3 className=' cursor-default text-[2rem] leading-[1.25] font-medium text-[#FFFFF] justify-center items-center  '>สูจิบัตร</h3>
             <a href="https://online.fliphtml5.com/exlay/hleh/" target='_blank' className='inline-flex justify-center items-center bg-[#c1788b] text-xl text-white px-10 mt-5 py-3 rounded-md'  >E-book</a>
         </div>
         <div >
