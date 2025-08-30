653f2ad1d55682bf96f213dfaa5c6d1a4df05507
ford456 <fordllawat@gmail.com>
2025-08-30 18:05:56 +0700

    up

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -3,7 +3,7 @@ import bannerhome from '../assets/img/bannerhome.png'
 
 export default function Banner() {
   return (
-    <div className='bg-cover max-w-[1320px] h-auto my-10  pt-10 md:py-5 '>
+    <div className='bg-cover h-auto my-10  pt-10 md:py-5 '>
         <img className=' w-full' src={bannerhome} alt="" />
     </div>
     
